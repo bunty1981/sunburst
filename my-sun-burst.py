2cf39d385c68386254072830a70d84acaa661e77
bunty1981 <roshni.b@gmail.com>
2025-09-30 08:46:41 -0500

    modified html output path

--- my-sun-burst.py
+++ my-sun-burst.py
@@ -1,3 +1,9 @@
+#! ~/.anaconda3/envs/sunburst/bin/python
+
+# This script creates a sunburst chart from a CSV file and saves it as an HTML file.
+# >> conda activate sunburst # to activate the conda environment, run command in terminal
+
+
 import pandas as pd
 df = pd.read_csv("sample_data.csv")
 print(df.columns)
@@ -5,5 +11,6 @@ print(df.columns)
 import plotly.express as px
 # fig is commonly used as convention in plotly figures
 fig = px.sunburst(df, path=['Category', 'Level 1','Level 2'], values ='Total', title ='Budget Breakdown')
-
 fig.show()
+
+fig.write_html("outputs/my-sun-burst.html")
